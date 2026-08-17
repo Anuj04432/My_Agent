6fa141fb5b624d80a8542ce0642cd7a0f0f2d141
Anuj04432 <anujwagmore835@gmail.com>
2026-08-17 06:07:30 +0530

    Fix: datetime error

--- tools/my_datetime.py
+++ tools/my_datetime.py
@@ -10,23 +10,3 @@ def get_datetime(query:str):
     now = datetime.now()
     return now
 
-# llm = ChatOllama(model="qwen2.5:3b-instruct")
-
-# agent = create_agent(model=llm, tools=[get_datetime],
-#                      system_prompt= """When the user asks for the current date or time, "
-#         "use the get_datetime tool. "
-#         "Return the result in a clear format: 
-#         {"date":
-#         "timr":
-#         }
-#         """)
-
-# response = agent.invoke({
-#     "messages":{
-#         "role":"user",
-#         "content":"What is the current date and time"
-#     }
-# })
-
-# print(response["messages"][-1].content)
-
