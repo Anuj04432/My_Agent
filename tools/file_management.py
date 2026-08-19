af790ff405af5ae3faeb859d2fd4beed16319af9
Anuj04432 <anujwagmore835@gmail.com>
2026-08-19 14:58:12 +0530

    feat :get_folder and get_directory are created

--- tools/file_management.py
+++ tools/file_management.py
@@ -13,29 +13,18 @@ def get_files(filename:str):
 
     return f"{filename} not found"
 
-llm = ChatOllama(model="qwen2.5:3b-instruct")
+def get_folder(folder_name:str):
+    for root,directory,file in os.walk("C:\\"):
+        if folder_name in directory:
+            path = os.path.join(root,folder_name)
+            return path
 
-agent = create_agent(
-    model=llm,
-    tools=[get_files],
-    system_prompt = """
-You are a file management assistant.
-
-Use the available tools to find, create, move, copy, or delete files and folders.
-
-When the user asks to find a file, use the file search tool.
-When the user asks to find a folder, use the folder search tool.
-
-Always give the full path of the file or folder.
-""")
-
-query = input("Enter the prompt:")
-response = agent.invoke({
-    "messages":{
-        "role":"user",
-        "content":query
-    }
-})
+def get_directory(folder,file):
+    for root,dirs,files in os.walk("C:\\"):
+        if folder in dirs:
+            path = os.path.basename(root) == folder
+            if file in files:
+                return os.path.join(root,file)
+    return "File not found"      
 
 
-print(response["messages"][-1].content)
