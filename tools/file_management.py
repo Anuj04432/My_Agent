ccf5c130b2bdf088369cedfc4357f623c166569d
Anuj04432 <anujwagmore835@gmail.com>
2026-08-26 19:21:02 +0530

    fix: The tool calling issue

--- tools/file_management.py
+++ tools/file_management.py
@@ -13,20 +13,25 @@ def get_files(filename:str):
 
     return f"{filename} not found"
 
+@tool
 def get_folder(folder_name:str):
+    """Get the folder location"""
     for root,directory,file in os.walk("C:\\"):
         if folder_name in directory:
             path = os.path.join(root,folder_name)
             return path
-
+        
+@tool
 def get_directory(folder,file):
+    """Get the location of a directory."""
     for root,dirs,files in os.walk("C:\\"):
         
         if os.path.basename(root) == folder:
             if file in files:
                 return os.path.join(root,file)
-    return "File not found"      
-
+    return "File not found"  
+    
+@tool
 def create_file(filename: str, content: str):
     """Create a file and write the content"""
     if os.path.exists(filename):
@@ -36,12 +41,12 @@ def create_file(filename: str, content: str):
 
     return f"{filename} file has created successfully"
 
-def delete_file():
-    file = input("Enter the file name: ")
+@tool
+def delete_file(file):
+    """Delete the file the directory"""
+    
     if os.path.exists(file):
         os.remove(file)
         return f"{file} file deleted"
     else:
         return f"{file} file not exist"
-
-print(get_directory("tools", "git_tool.py"))
